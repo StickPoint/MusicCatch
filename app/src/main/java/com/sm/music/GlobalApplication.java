80ce917192f238a1069b36956b185f58f8c81c3f
刘禹辰 <42709452+LegendLiuYC@users.noreply.github.com>
2021-02-12 14:12:44 +0800

    Updata by Nickers

--- app/src/main/java/com/sm/music/GlobalApplication.java
+++ app/src/main/java/com/sm/music/GlobalApplication.java
@@ -40,6 +40,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.Random;
 
+
+
 public class GlobalApplication extends Application {
 
 
@@ -91,6 +93,24 @@ public class GlobalApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+//        XUpdate.get()
+//                .debug(true)
+//                .isWifiOnly(true)                                               //默认设置只在wifi下检查版本更新
+//                .isGet(true)                                                    //默认设置使用get请求检查版本
+//                .isAutoMode(false)                                              //默认设置非自动模式，可根据具体使用配置
+//                .param("versionCode", UpdateUtils.getVersionCode(this))         //设置默认公共请求参数
+//                .param("appKey", getPackageName())
+//                .setOnUpdateFailureListener(new OnUpdateFailureListener() {     //设置版本更新出错的监听
+//                    @Override
+//                    public void onFailure(UpdateError error) {
+//                        if (error.getCode() != CHECK_NO_NEW_VERSION) {          //对不同错误进行处理
+//                            ToastUtils.toast(error.toString());
+//                        }
+//                    }
+//                })
+//                .supportSilentInstall(true)                                     //设置是否支持静默安装，默认是true
+//                .setIUpdateHttpService(new OKHttpUpdateHttpService())           //这个必须设置！实现网络请求功能。
+//                .init(this);
         conn = new GetMusic();
         musicList = RecentPlay.getRecentPlayMusic(getApplicationContext());
         Intent musicIntent = new Intent(getApplicationContext(), MusicPlayer.class);

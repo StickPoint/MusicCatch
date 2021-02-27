d3376af73e03396c902817d867b91344e4b584ae
刘禹辰 <42709452+LegendLiuYC@users.noreply.github.com>
2021-02-27 17:20:31 +0800

    Updata by Nickers 2021.2.12

--- app/src/main/java/com/sm/music/Server/MusicPlayer.java
+++ app/src/main/java/com/sm/music/Server/MusicPlayer.java
@@ -3,6 +3,7 @@ package com.sm.music.Server;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
@@ -12,6 +13,10 @@ import android.os.IBinder;
 import android.os.SystemClock;
 import android.widget.RemoteViews;
 
+import androidx.core.app.NotificationCompat;
+import androidx.media.app.NotificationCompat.DecoratedMediaCustomViewStyle;
+
+import com.sm.music.Activity.PlayerActivity;
 import com.sm.music.GlobalApplication;
 import com.sm.music.R;
 
@@ -39,18 +44,21 @@ public class MusicPlayer extends Service {
     public void onCreate() {
         super.onCreate();
         globalApplication = (GlobalApplication) getApplication();
+
         NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-        Notification.Builder builder = new Notification.Builder(getApplicationContext())
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_LOW);
+            manager.createNotificationChannel(channel);
+        }
+        Notification builder = new NotificationCompat.Builder(getApplicationContext(), CHANNEL_ID)
 //                .setCustomContentView(new RemoteViews(getPackageName(), R.layout.notification_layout))
                 .setWhen(SystemClock.currentThreadTimeMillis())
                 .setSmallIcon(R.mipmap.ic_launcher_round)
-                .setOngoing(true);
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
-            NotificationChannel channel = new NotificationChannel(CHANNEL_ID,CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT);
-            manager.createNotificationChannel(channel);
-            builder.setChannelId(CHANNEL_ID);
-        }
-        startForeground(1, builder.build());
+                .setPriority(Notification.VISIBILITY_PUBLIC)
+//                .setContentIntent(PendingIntent.getActivity(this, 0, new Intent(this, PlayerActivity.class), PendingIntent.FLAG_NO_CREATE))
+                .setOngoing(true)
+                .build();
+        startForeground(1, builder);
         player = new MediaPlayer();
     }
 
